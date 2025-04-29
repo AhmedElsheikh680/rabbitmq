e5b5d5d08e0131b05ffdb0b90cd010c2a8c5ef4c
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2025-04-28 21:10:13 -0700

    init project & set configuration

---
+++ src/main/java/com/spring/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,46 @@
+package com.spring.rabbitmq.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+    @Value("${spring.rabbitmq.virtual-host}")
+    private String virtualHost;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        AbstractConnectionFactory abstractConnectionFactory = new CachingConnectionFactory();
+        abstractConnectionFactory.setHost(host);
+        abstractConnectionFactory.setPort(port);
+        abstractConnectionFactory.setVirtualHost(virtualHost);
+        abstractConnectionFactory.setUsername(username);
+        abstractConnectionFactory.setPassword(password);
+
+        return abstractConnectionFactory;
+    }
+    // convert message that come form rabbitmq to project to json
+    @Bean
+
+    public MessageConverter messageConverter () {
+        ObjectMapper objectMapper = new ObjectMapper().findAndRegisterModules();
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
