d563441931888ec519e9003fab2e184bc7674546
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2025-05-06 23:47:29 -0700

    create consume service

---
+++ src/main/java/com/spring/rabbitmq/service/ComsumeService.java
@@ -0,0 +1,36 @@
+package com.spring.rabbitmq.service;
+
+import com.spring.rabbitmq.model.Message;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Service
+public class ComsumeService {
+
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public int getCountMessage(String queueName) {
+       Properties properties =  amqpAdmin.getQueueProperties(queueName);
+       return (int) properties.get(RabbitAdmin.QUEUE_MESSAGE_COUNT);
+    }
+
+    public List<Message> receiveMessage(String queueName) {
+        int count = getCountMessage(queueName);
+        return IntStream.range(0, count)
+                .mapToObj(value -> (Message)rabbitTemplate.receiveAndConvert(queueName))
+                .collect(Collectors.toList());
+    }
+}
